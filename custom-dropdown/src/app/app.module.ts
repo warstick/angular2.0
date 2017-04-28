70db37d832bdd33038064037be89496a00bca5bd
Mani <manikanta.srirangam@gmail.com>
2017-04-28 14:52:48 +0530

    Adding Custom Dropdown Component

--- custom-dropdown/src/app/app.module.ts
+++ custom-dropdown/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CustomDropdownComponent } from './custom-dropdown/custom-dropdown.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomDropdownComponent
   ],
   imports: [
     BrowserModule,
---
+++ custom-dropdown/src/app/custom-dropdown/custom-dropdown.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-dropdown',
+  templateUrl: './custom-dropdown.component.html',
+  styleUrls: ['./custom-dropdown.component.css']
+})
+export class CustomDropdownComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
